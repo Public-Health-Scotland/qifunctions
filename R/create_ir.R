4c246ec9a7f438144f48391a5f56fb4b7ffd1305
Jaime Villacampa <jaime.villacampa@phs.scot>
2022-07-06 10:12:57 +0100

    adding source and title

--- R/create_ir.R
+++ R/create_ir.R
@@ -1,19 +1,25 @@
 #
-#' @title SMR01 completeness
+#' @title Create IR folder and templates
 #'
 #' @description \code{create_ir} Creates folder, code and output template for an
 #' information request folders and opens a new project for the IR.
 #'
-#' @details Everything is created in cl-out
+#' @details Everything is created in cl-out.
 #'
 #' @param ir_number A \code{character} string specifying the name of the IR,
 #' including the year, e.g. IR2022-5555.
 #'
+#' @param title An optional \code{character} string specifying the title of the IR.
+#'
+#' @param source An optional \code{character} string specifying the source of the data.
+#'
 #' @return Creates a series of directories in cl-out, a script that can be used
 #' as a template for the IR, an Excel file for the final output, and opens the project
-#' in a new window
+#' in a new window.
 #'
-#' @example create_ir("IR2022-55555")
+#' @examples create_ir("IR2022-55555")
+#'
+#' create_ir("IR2022-55555", title = "Number of admissions", source = "SMR01")
 #'
 #' @importFrom magrittr %>%
 #' @importFrom magrittr %<>%
@@ -21,11 +27,11 @@
 #' @export
 
 
-create_ir <- function(ir_number) {
+create_ir <- function(ir_number, title = NULL, source = NULL) {
 
   ir_year <- sub("-.*", "", ir_number)
 
-  Sys.umask("002") # to set permissions right
+  Sys.umask("000") # to set permissions right
 
   ###############################################.
   ## Creating directories ----
@@ -99,7 +105,7 @@ create_ir <- function(ir_number) {
 
     ###END"
 
-    writeLines(ir_template, paste0(ir_folder, "/code/", ir_number , ".R"))
+    writeLines(ir_template, ir_script)
 
   } else {
     print("Script for this IR already exists")
@@ -109,19 +115,36 @@ create_ir <- function(ir_number) {
   ## Bringing IR template ----
   ###############################################.
 
-  irtemplate <- openxlsx::loadWorkbook(system.file("extdata", "ir-template.xlsx",
+  ir_excel <- openxlsx::loadWorkbook(system.file("extdata", "ir-template.xlsx",
                                                    package = "qifunctions"))
 
   # Writing reference number in function
-  openxlsx::writeData(irtemplate, "Notes", paste("Ref:", ir_number),
+  openxlsx::writeData(ir_excel, "Notes", paste("Ref:", ir_number),
                       startCol = 2, startRow = 3)
 
   # Writing month of extract based on current date
-  openxlsx::writeData(irtemplate, "Notes",
+  openxlsx::writeData(ir_excel, "Notes",
                       paste("Date extracted: ", format(Sys.Date(), "%B %Y")),
                       startCol = 2, startRow = 4)
 
-  openxlsx::saveWorkbook(irtemplate, paste0(ir_folder, "/", ir_number , ".xlsx"))
+  # Adding title if specified in the function call
+  if (!is.null(title) ) {
+    openxlsx::writeData(ir_excel, "Notes", title,
+                        startCol = 2, startRow = 1)
+  }
+
+  # Adding source if specified in the function call
+  if (!is.null(source) ) {
+    openxlsx::writeData(ir_excel, "Notes",
+                        paste("Source:", source, "- Public Health Scotland"),
+                        startCol = 2, startRow = 2)
+  }
+
+  openxlsx::saveWorkbook(ir_excel, paste0(ir_folder, "/", ir_number , ".xlsx"))
+
+  ###############################################.
+  ## Creating new project ----
+  ###############################################.
 
   # Creating R project and opens in new window
   rstudioapi::openProject(ir_folder, newSession = TRUE)
